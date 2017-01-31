fb88ed1761fcb3508acfcbaef24cc3ecde92ee83
leila.donamaria <leila@contenidos-digitales.com>
2017-01-31 14:21:46 -0300

    overlay effect

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -10,5 +10,21 @@ $(document).ready(function () {
     $('.right-btn').click(function () {
         $('.slider').slick('slickNext');
     });
+
+    $('.overlay-btn').click(function () {
+        if ($('.overlay-pantalla').css('opacity') === '0') {
+            $('.overlay-pantalla').css('opacity', '0.7');
+            $('.overlay-btn').removeClass('gira');
+        } else {
+            $('.overlay-pantalla').css('opacity', '0');
+            $('.overlay-btn').addClass('gira');
+        }
+        // $('.developers-container .item-partner').toggleClass('animated');
+        // if($('.developers-container .item-partner').hasClass('animated')){
+        //     $('#plusBtn').addClass('gira');
+        // }else{
+        //     $('#plusBtn').removeClass('gira');
+        // }
+    });
 });
 //# sourceMappingURL=main.js.map
