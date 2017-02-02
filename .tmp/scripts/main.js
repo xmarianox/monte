e82e3fb86cdf5c8bc1f4e59ad4d1d9a0ad2fccf5
leila.donamaria <leila@contenidos-digitales.com>
2017-02-02 17:46:48 -0300

    slider 50%

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -35,12 +35,12 @@ $(document).ready(function () {
     $('.btn-hamburger').click(function () {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
-            $(".pantalla-mobile").css('opacity', '0');
-            $(".pantalla-mobile").css('height', '0');
+            $('.pantalla-mobile').css('opacity', '0');
+            $('.pantalla-mobile').css('height', '0');
         } else {
             $(this).addClass('active');
-            $(".pantalla-mobile").css('opacity', '1');
-            $(".pantalla-mobile").css('height', 'auto');
+            $('.pantalla-mobile').css('opacity', '1');
+            $('.pantalla-mobile').css('height', 'auto');
         }
     });
     mobileVsDesk();
